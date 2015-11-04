a28314fa3b23cdcac89f47bcef2e7b0f77893884
peter.wilkins.mayden <peter.wilkins@mayden.com>
2015-11-04 16:56:32 +0000

    first commit

---
+++ src/Pontoon.php
@@ -0,0 +1,80 @@
+<?php
+
+class Pontoon
+{
+
+    function scoreHand($hand)
+    {
+        $total = 0;
+        $ace = 0;
+        foreach ($hand as $card) {
+            switch ($card['name']) {
+                case 'ace':
+                    $ace++;
+                    break;
+                case 'king':
+                case 'queen':
+                case 'jack':
+                    $total += 10;
+                    break;
+                default:
+                    $total += $card['name'];
+            }
+        }
+       // echo "\n total = " . $total;
+       // echo "\n ace = " . $ace;
+        if($ace > 1){
+            $total += $ace;
+        }
+        elseif($ace == 1){
+            if($total <= 10){
+                $total += 11;
+            }
+            else{
+                $total += 1;
+            }
+        }
+        return $total;
+    }
+
+    function dealCards(&$fullDeck, $num)
+    {
+        while ($num > 0) {
+            $hand[] = array_pop($fullDeck);
+            $num --;
+        }
+
+        var_dump($hand);
+        return $hand;
+    }
+
+    function popCard(&$fullDeck)
+    {
+        return array_pop($fullDeck);
+    }
+
+    function newShuffledDeck()
+    {
+        $suitee = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace'];
+        $deck = [
+            'hearts'   => $suitee,
+            'diamonds' => $suitee,
+            'spades'   => $suitee,
+            'clubs'    => $suitee,
+        ];
+
+        foreach ($deck as $suit => $suitee) {
+            foreach ($suitee as $card) {
+                $fullDeck[] = [
+                    'suit' => $suit,
+                    'name' => $card,
+                ];
+            }
+        }
+        shuffle($fullDeck);
+
+        return $fullDeck;
+    }
+
+}
+
---
+++ tests/PontoonTest.php
@@ -0,0 +1,125 @@
+<?php
+
+
+class PontoonTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @param $hand
+     * @param $expectedResult
+     *
+     * @dataProvider handProvider
+     */
+    public function testDoubles($hand, $expectedResult)
+    {
+        $a = new Pontoon;
+        $this->assertEquals($expectedResult, $a->scoreHand($hand));
+    }
+
+    public static function handProvider()
+    {
+        return [
+            [
+                [
+                    ['suit' => "diamonds", 'name' => "2"],
+                    ['suit' => "spades", 'name' => "ace"],
+                ],
+                13,
+            ],
+            [
+                [
+                    ['suit' => "diamonds", 'name' => "ace"],
+                    ['suit' => "spades", 'name' => "ace"],
+                ],
+                2,
+            ],
+            [
+                [
+                    ['suit' => "diamonds", 'name' => "king"],
+                    ['suit' => "spades", 'name' => "ace"],
+                ],
+                21,
+            ],
+            [
+                [
+                    ['suit' => "diamonds", 'name' => "2"],
+                    ['suit' => "spades", 'name' => "ace"],
+                    ['suit' => "spades", 'name' => "ace"],
+                    ['suit' => "spades", 'name' => "ace"],
+                    ['suit' => "spades", 'name' => "ace"],
+                ],
+                6,
+            ],
+            [
+                [
+                    ['suit' => "diamonds", 'name' => "5"],
+                    ['suit' => "spades", 'name' => "5"],
+                    ['suit' => "spades", 'name' => "5"],
+                    ['suit' => "spades", 'name' => "5"],
+                    ['suit' => "spades", 'name' => "ace"],
+                ],
+                21,
+            ],
+            [
+                [
+                    ['suit' => "spades", 'name' => "9"],
+                    ['suit' => "spades", 'name' => "ace"],
+                    ['suit' => "spades", 'name' => "ace"],
+                ],
+                21,
+            ],
+            [
+                [
+                    ['suit' => "spades", 'name' => "8"],
+                    ['suit' => "spades", 'name' => "ace"],
+                    ['suit' => "spades", 'name' => "ace"],
+                ],
+                19,
+            ],
+        ];
+    }
+
+    function test_create_a_new_pack_of_shuffled_cards()
+    {
+        $a = new Pontoon;
+        $deck = $a->newShuffledDeck();
+        $this->assertEquals(52, count($deck));
+    }
+
+    function test_popCard_returns_an_array_with_two_elements()
+    {
+        $a = new Pontoon;
+        $deck = $a->newShuffledDeck();
+        $card = $a->popCard($deck);
+        $this->assertEquals(2, count($card));
+    }
+
+    function test_dealCards_returns_number_of_cards_in_argument()
+    {
+        $a = new Pontoon;
+        $deck = $a->newShuffledDeck();
+        $hand = $a->dealCards($deck, 2);
+        $this->assertEquals(2, count($hand));
+
+    }
+
+    function test_dealCards_removes_from_deck_number_of_cards_in_argument()
+    {
+        $a = new Pontoon;
+        $deck = $a->newShuffledDeck();
+        $hand = $a->dealCards($deck, 2);
+        $this->assertEquals(2, count($hand));
+
+    }
+
+    function test_scoreHand_returns_sum_of_value_of_cards_in_hand()
+    {
+        $a = new Pontoon;
+        $hand = [
+            ['suit' => "diamonds", 'name' => "2"],
+            ['suit' => "spades", 'name' => "ace"],
+        ];
+        $this->assertEquals(13, $a->scoreHand($hand));
+    }
+
+}
